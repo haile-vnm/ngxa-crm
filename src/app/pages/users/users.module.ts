4eefe8265b4b90d9bb4e306912a428c97a54e7f2
Hai Le <haile.vnm@gmail.com>
2023-10-11 16:59:31 +0700

    chore: add antd (ng-zorro)

---
+++ src/app/pages/users/users.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+
+import { UsersRoutingModule } from './users-routing.module';
+
+import { UsersComponent } from './users.component';
+
+
+@NgModule({
+  imports: [UsersRoutingModule],
+  declarations: [UsersComponent],
+  exports: [UsersComponent]
+})
+export class UsersModule { }
